e0cf55134b188d50a4925d32be9705331b8104b3
Giovanni Arroyo <giovanni@imagescape.com>
2016-03-17 14:45:37 -0500

    create setup.py file, update .gitignore

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='django_payflow',
+      version='0.1',
+      description="A django app for interaction with the django_payflow api",
+      install_requires=(
+          'requests',
+      ),
+)
